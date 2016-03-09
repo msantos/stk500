768ecf3a932e1f8a6a7831ec226084196c3aaa53
Michael Santos <michael.santos@gmail.com>
2016-03-09 06:56:07 -0500

    test: handle serial port in init per test case

--- test/stk500_SUITE.erl
+++ test/stk500_SUITE.erl
@@ -33,30 +33,79 @@
 -include_lib("common_test/include/ct.hrl").
 
 -export([
-        all/0
+        all/0,
+        groups/0,
+        init_per_suite/1,
+        end_per_suite/1,
+        init_per_testcase/2,
+        end_per_testcase/2
     ]).
 
 -export([
-        stk500_program_load/1
+        stk500_program_load/1,
+        counter/1
     ]).
 
 -define(HEX_FILE, "counter.cpp.hex").
 
+-define(INCR, 0).
+-define(READ, 1).
+-define(SET, 2).
+-define(INT16(N), N:2/little-integer-unit:8).
+
 all() ->
-    [stk500_program_load].
+    [{group, load}].
+
+groups() ->
+    [{load, [sequence], [stk500_program_load]}].
+
+init_per_suite(Config) ->
+    Serial = stk500:serial_device(),
+    [{serial, Serial}|Config].
+
+end_per_suite(Config) ->
+    Config.
+
+init_per_testcase(_Test, Config) ->
+    Serial = ?config(serial, Config),
+    {ok, FD} = stk500:open(Serial),
+    [{fd, FD}|Config].
+
+end_per_testcase(_Test, Config) ->
+    FD = ?config(fd, Config),
+    ok = serctl:close(FD),
+    Config.
 
 %% Upload code to an Arduino.
-stk500_program_load(_Config) ->
-    Serial = stk500:serial_device(),
-    {ok,FD} = stk500:open(Serial),
+stk500_program_load(Config) ->
+    FD = ?config(fd, Config),
     error_logger:info_report([{hex_file, codepath(?HEX_FILE)}]),
     Hex = stk500:hex_file(codepath(?HEX_FILE)),
     Bytes = stk500:chunk(Hex, 128),
-    ok = stk500:load(FD, Bytes, [verbose]),
-    ok = serctl:close(FD).
+    ok = stk500:load(FD, Bytes, [verbose]).
+
+counter(Config) ->
+    FD = ?config(fd, Config),
+    Termios = lists:foldl(
+        fun(Fun, Acc) -> Fun(Acc) end,
+            serctl:mode(raw),
+            [
+                fun(N) -> serctl:flow(N, false) end,
+                fun(N) -> serctl:ispeed(N, b9600) end,
+                fun(N) -> serctl:ospeed(N, b9600) end
+            ]
+        ),
+    ok = serctl:tcsetattr(FD, tcsanow, Termios),
+
+    % Increment the counter
+    ok = serctl:write(FD, <<?INT16(?INCR), ?INT16(1)>>),
+
+    % Request the new value
+    serctl:write(FD, <<?INT16(?READ), ?INT16(0)>>),
+    {ok, <<2:2/integer-unit:8, ?INT16(1)>>} = serctl:readx(FD, 4).
 
 codepath(File) ->
     filename:join([
             filename:dirname(code:which(?MODULE)),
             File
-            ]).
+        ]).
